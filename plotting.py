eaa24940bce3e235d16154fb4ebca6c161d6eb6e
Adam <damskiwood@gmail.com>
2024-02-06 17:32:56 +0000

    PLOTS

--- plotting.py
+++ plotting.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-name = "Simulation_Data_Glauber_main"
+name = "Simulation_Data_Kawasaki_main"
 
 df = pd.read_csv(name)
 T = np.linspace(1,3,20)
@@ -28,14 +28,21 @@ for i in col:
 """""
 fig1 = plt.figure("Figure 1")
 plt.errorbar(T,df["Specific Heat"],yerr=df["Error c boot"],capsize=2, markeredgewidth=1)
-plt.title("Bootstrap")
+plt.title("Specific Heat with bootstrap error calculation")
+plt.xlabel("Temperature (K)")
+plt.ylabel("Specific Heat")
+
 fig2 = plt.figure("Figure 2")
 plt.errorbar(T,df["Specific Heat"],yerr=df["Error c jack"],capsize=2, markeredgewidth=1)
-plt.title("Jacknife")
+plt.title("Specific Heat with jacknife error calculation")
+plt.xlabel("Temperature (K)")
+plt.ylabel("Specific Heat")
+
 fig3 = plt.figure("Figure 3")
 plt.plot(T,df["Energy"])
 plt.title("Energy")
 plt.xlabel("Temperature (K)")
 plt.ylabel("Energy")
+
 plt.show()
 #Error c jack = Error x boot
