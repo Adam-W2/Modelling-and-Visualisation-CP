908026476b05ad64fd99d90cba1ca2245e19d51f
Adam <damskiwood@gmail.com>
2024-04-01 17:21:08 +0100

    final

--- Checkpoint 3/Cahn-Hilliard/main.py
+++ Checkpoint 3/Cahn-Hilliard/main.py
@@ -8,7 +8,7 @@ N = 100
 dx = 1
 dt = 1
 nsteps = 100000
-phi_zero = 0
+phi_zero = 0.5
 
 free = []
 steps = []
@@ -23,14 +23,15 @@ for i in range(nsteps):
     if i % 10 == 0:
         free.append(grid.free_energy())
         steps.append(i)
-        plt.cla()
-        im = plt.imshow(grid.grid_phi, animated=True)
-        plt.draw()
-        plt.pause(0.00001)
+        #plt.cla()
+        #im = plt.imshow(grid.grid_phi, animated=True)
+        #plt.draw()
+        #plt.pause(0.00001)
     n += 1
 df = pd.DataFrame({"Free energy":free,"step":steps})
-df.to_csv("Data/test2_100000.csv",index=False)
+df.to_csv("Data/free_energy_05_100000.csv",index=False)
 
 fig2 = plt.figure("Figure 2")
 plt.plot(steps,free)
+plt.title("phizero = 0.5")
 plt.show()
