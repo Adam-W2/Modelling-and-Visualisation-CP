33599371fd72537ca5a94ff9e8906322babe1938
Adam <damskiwood@gmail.com>
2024-04-22 13:27:53 +0100

    last bit

---
+++ Exam revision/2022/main2022.py
@@ -0,0 +1,39 @@
+import matplotlib
+matplotlib.use("TKAgg")
+import matplotlib.pyplot as plt
+from Grid import Grid
+import pandas as pd
+
+N,dx,dt,nsteps,phi_zero = input("Please input N, dx, dt number of steps and phi zero: ").split(" ")
+
+N = int(N)
+dx = float(dx)
+dt = float(dt)
+nsteps = int(nsteps)
+phi_zero = float(phi_zero)
+
+free = []
+steps = []
+n = 0
+
+grid = Grid(dx,dt,phi_zero,N)
+grid.create_phi_grid()
+
+for i in range(nsteps):
+    grid.steps()
+    if i % 10 == 0:
+        free.append(grid.free_energy())
+        steps.append(i)
+        plt.cla()
+        im = plt.imshow(grid.grid_phi, animated=True)
+        plt.draw()
+        plt.pause(0.00001)
+
+    n += 1
+df = pd.DataFrame({"Free energy":free,"step":steps})
+df.to_csv("Data/free_energy_05_100000.csv",index=False)
+
+fig2 = plt.figure("Figure 2")
+plt.plot(steps,free)
+plt.title("phizero = 0.5")
+plt.show()
